9870c8a0e95585c8eaa5a9c7fd60b3f8e732b719
Ravshan <ravkart@gmail.com>
2019-06-13 22:55:46 -0700

    basic functions for GameViewController v1

--- MergeBlocks/Controller/GameViewController.swift
+++ MergeBlocks/Controller/GameViewController.swift
@@ -18,4 +18,40 @@ class GameViewController: UIViewController {
     var score = 0
     var isGameOver = false
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupScene()
+        beginGame()
+    }
+    func setupScene(){
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFit
+        
+        level = Level()
+        scene.level = level
+        scene.addTiles()
+        scene.tapHandler = handleFalling
+        skView.presentScene(scene)
+    }
+    
+    func beginGame() {
+        score = 0
+        //  gameOver()
+        numberOfBox = 0
+        level.cleanArena()
+        scene.removeAllBoxSprites()
+        updateLabels()
+        isGameOver = false
+        shuffle()
+    }
+    
+    func shuffle() {
+        let newBox = level.newBox()
+        scene.addSprite(for: newBox)
+        scene.animateSwingBox(box: newBox)
+    }
 }
